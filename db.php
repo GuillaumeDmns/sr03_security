90254033ac3557b76430d9539ae8620d8067fd16
Natan Danous <natan.danous@gmail.com>
2019-04-05 11:30:01 +0200

    Fix sql injection for auth

--- db.php
+++ db.php
@@ -1,8 +1,18 @@
 <?php
 define('DB_HOST','127.0.0.1');
 define('DB_USER','root');
-define('DB_PASSWD','');
+define('DB_PASSWD','natandanous');
 define('DB_NAME','sr03');
+
+try {
+    $pdo = new PDO("mysql:host=127.0.0.1;dbname=sr03", 'root', 'natandanous');
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+return $pdo;
 
 function query($q) {
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWD, DB_NAME);
--- model/users.php
+++ model/users.php
@@ -2,15 +2,16 @@
 require_once('db.php');
 
 function findUserByLoginPwd($login, $pwd) {
-  $result = query("select nom,prenom,login,id_user,numero_compte,profil_user,solde_compte from USERS where login='$login' and mot_de_passe='$pwd'");
+  global $pdo;
+  $stmt = $pdo->prepare("select nom,prenom,login,id_user,numero_compte,profil_user,solde_compte from USERS where login=:login and mot_de_passe=:pwd");
+  $stmt->execute(['login'=>$login,'pwd'=>$pwd]);
+  $result = (array) $stmt->fetchObject();
 
-  if ($result->num_rows === 0) {
-    $utilisateur = false;
+  if (!$result[0]) {
+    return false;
   } else {
-    $utilisateur = $result->fetch_assoc();
+    return $result;
   }
-
-  return $utilisateur;
 }
 
 function findUserByAccount($numero_compte) {
